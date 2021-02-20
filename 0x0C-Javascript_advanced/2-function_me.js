2b294b2c4481e52e5b4940a7ff7fbf745ed0a9e3
Diego Tardio <diego@Diegos-MacBook-Pro.local>
2021-02-20 16:45:26 -0500

    [TASK 2. Closure]

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+function welcomeMessage(fullName) {
+    alert("Welcome " + fullName);
+}
+
+var guillaume = function () {welcomeMessage("Guillame")};
+var alex = function () {welcomeMessage("Alex")};
+var fred = function () {welcomeMessage("Fred")};
