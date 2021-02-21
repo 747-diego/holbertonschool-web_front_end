e2a167ff876130714a3949b192d673b5045f9d3e
Diego Tardio <diego@Diegos-MacBook-Pro.local>
2021-02-21 18:53:33 -0500

    [TASK 7. Stack order and setTimeout]

---
+++ 0x0C-Javascript_advanced/7-timeout.js
@@ -0,0 +1,8 @@
+console.log("Start of the execution queue");
+console.log(setTimeout("Final code block to be executed"));
+
+for(let index = 0; index < 100; index++) {
+    console.log(index);
+}
+
+console.log("End of the loop printing");
